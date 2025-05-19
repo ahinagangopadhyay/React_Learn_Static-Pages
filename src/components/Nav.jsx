f2d78d49716cf441db56df5c1248040b883e665e
ahinagangopadhyay <ag.23cs8082@nitdgp.ac.in>
2025-05-19 16:48:23 +0530

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+export default function Nav()
+
+{
+  return(     <header>
+            <nav>
+                <img src="/logo512.png" alt="React logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>)
+
+}
